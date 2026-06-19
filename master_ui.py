df26e178be08b375a0b6ac4a3a2cc1bc736754fd
hp25hnk2t8-beep <hp25hnk2t8@privaterelay.appleid.com>
2026-06-19 19:14:31 +0400

    Update master_ui.py

--- master_ui.py
+++ master_ui.py
@@ -994,4 +994,4 @@ if __name__ == "__main__":
     print("   💰 Total Balance - բոլոր բալանսների գումարը")
     print("   🔄 Retry - վերագործարկել ակաունթը բոլոր սերվերների վրա")
     print("=" * 60 + "\n")
-    uvicorn.run(app, host="0.0.0.0", port=9000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=9000, ssl_keyfile="key.pem", ssl_certfile="cert.pem", log_level="info")
